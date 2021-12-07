b102643f9ba86f5df722779a44bc0255b9325c59
Jiaxin Liu <60685242+victorliu-sq@users.noreply.github.com>
2021-12-07 16:56:20 -0500

    Update generator_model.py

--- code/generator_model.py
+++ code/generator_model.py
@@ -61,12 +61,15 @@ class Generator(nn.Module):
             x = layer(x)
         return torch.tanh(self.last(x))
 
-def test():
-    img_channels = 3
-    img_size = 256
-    x = torch.randn((2, img_channels, img_size, img_size))
-    gen = Generator(img_channels, 9)
-    print(gen(x).shape)
+    """
+    def test():
+        img_channels = 3
+        img_size = 256
+        x = torch.randn((2, img_channels, img_size, img_size))
+        gen = Generator(img_channels, 9)
+        print(gen(x).shape)
 
-if __name__ == "__main__":
-    test()
+    if __name__ == "__main__":
+        test()
+        
+    """
